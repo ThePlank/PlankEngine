8190f5a9d4b781f914c67506a6b5a4880728f763
ArdaBeyNotFound <98959759+ArdoDev-stupid@users.noreply.github.com>
2022-12-25 13:54:33 +0300

    hopefully fix now for notesplashes

--- source/display/objects/NoteSplash.hx
+++ source/display/objects/NoteSplash.hx
@@ -10,7 +10,7 @@ class NoteSplash extends FlxSprite {
 
         super(x, y);
 
-        frames = Paths.getSparrowAtlas('noteSplashes');
+        frames = Paths.getSparrowAtlas('noteSplashes', 'shared');
         animation.addByPrefix("note1-0", "note impact 1  blue", 24, false);
         animation.addByPrefix("note2-0", "note impact 1 green", 24, false);
         animation.addByPrefix("note0-0", "note impact 1 purple", 24, false);
